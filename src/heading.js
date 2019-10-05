3e2e09a4dec36484d19d161223a0dfca811f0dc3
Bane <bane.dd@gmail.com>
2019-10-05 03:05:40 -0400

    Added a heading and updated the styles

---
+++ src/heading.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = ({ title }) => {
+  return <h3>{title}</h3>;
+};
+
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import Heading from "./heading";
 import Stopwatch from "./stopwatch";
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <Heading title="stopwatch" />
       <Stopwatch />
     </div>
   );
